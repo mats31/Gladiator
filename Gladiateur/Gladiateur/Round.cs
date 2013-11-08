6d87521f2ebcc7ad9acb0b82f847639923f58226
buitonii <bui.bernard1@gmail.com>
2013-11-08 22:27:33 +0100

    Classe round

---
+++ Gladiateur/Gladiateur/Round.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gladiateur
+{
+    class Round
+    {
+    //CE QU'ON DOIT FAIRE MATHOU :)
+        //Premier combat
+            //lance combat : gla1[0] vs gla1[0]
+
+
+        //Mort gladiateur    
+            //si gla1[i] est mort (Combat)
+            //gla1[i+1] vs gla2[j]
+
+            //si gla2[i] est mort (Combat)
+            //gla1[i] vs gla2[j+1]
+
+
+        //Fin combat 
+            //si gla1[i] == 3
+            //joueur2 gagne
+            //joueur2 victoire++ et match_joue++
+            //joueur1 défaite++ et match_joue++
+        
+
+            //si gla2[i] == 3
+            //joueur1 gagne
+            //joueur1 victoire++ et match_joue++
+            //joueur2 défaite++ et match_joue++
+
+
+
+
+        //Attributs
+        private Equipe _eq1;
+        private Equipe _eq2;
+        private List<Gladiateur> _listeGladiateur1 = new List<Gladiateur>();
+        private List<Gladiateur> _listeGladiateur2 = new List<Gladiateur>();
+
+        //Constructeur
+        public Round(Equipe eq1, Equipe eq2){
+            this._eq1 = eq1;
+            this._eq2 = eq2;
+        }
+
+        //Méthode
+        public void recupEquipe()
+        {
+            //Récupération gladiateurs équipe 1
+            foreach (Gladiateur b_gla in this._eq1.ListeGladiateur)
+            {
+                _listeGladiateur1.Add(b_gla);
+            }
+
+            //Récupération gladiateurs équipe 2
+            foreach (Gladiateur b_gla in this._eq2.ListeGladiateur)
+            {
+                _listeGladiateur2.Add(b_gla);
+            }
+        }
+
+        public void firstRound() //lance le 1e combat : gla1[0] vs gla2[0]
+        {
+            Combat l_bataille1 = new Combat(_listeGladiateur1[0], _listeGladiateur2[0]);
+            l_bataille1.triArmeAttDef();
+            l_bataille1.Attaquer();
+        }
+
+//ICI
+//Y'A
+//UN TRUC
+        //CHER MATHOU, JE ME SUIS ARRETE ICI DANS MON CODE
+        //Je compte appelé cette méthode depuis la classe combat lorsqu'un gladiateur meurt
+        //J'ai pensé cette méthode en 3 étapes : (mais j'ai pas encore réfléchi si c'est faisable)
+        //étape1 : je récupère l'attribut gla1 ou gla2 envoyé en paramètre
+        //étape2 : je teste si ce gladiateur correspond à un gladiateur de _listeGladiateur1 ou de _listeGladiateur2 (avec un foreach)
+        //étape3 : j'appelle la méthode nextRound() avec comme paramètre le gladiateur qui appartient à l'une des deux listes
+        public void recupGlaMort(Gladiateur glaMort) //récupère le glaMort et vérifie si il fait partie de eq1 ou eq2 (on l'appelera dans la classe Combat)
+        {
+
+        }
+
+        public void nextRound() //quand un gla meurt, lance le combat suivant
+        {
+
+        }
+    }
+}
